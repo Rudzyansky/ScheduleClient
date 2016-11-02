9847c94e3e1d19232bf92738fca565422ee48705
Prog <prog32bit@ya.ru>
2016-11-02 10:33:18 +0300

    Модернизация Access Denied

--- app/src/main/java/ru/falseteam/schedule/socket/commands/AccessDenied.java
+++ app/src/main/java/ru/falseteam/schedule/socket/commands/AccessDenied.java
@@ -20,7 +20,7 @@ public class AccessDenied extends CommandAbstract {
         new Handler(Looper.getMainLooper()) {
             @Override
             public void handleMessage(Message inputMessage) {
-                Toast.makeText(Worker.get().getContext(), "Access Denied: \n" + map.get("command"), Toast.LENGTH_SHORT).show();
+                Toast.makeText(Worker.get().getContext(), "Access Denied: \n" + map.get("forbidden"), Toast.LENGTH_LONG).show();
             }
         }.obtainMessage().sendToTarget();
     }
