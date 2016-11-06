b4e60ff73c085f93b3861020904c77465866083d
VladislavSumin <vladislavsumin@mail.ru>
2016-11-06 18:31:46 +0300

    Оно даже работает

--- app/src/main/java/ru/falseteam/schedule/management/EditTemplateActivity.java
+++ app/src/main/java/ru/falseteam/schedule/management/EditTemplateActivity.java
@@ -8,6 +8,10 @@ import android.view.View;
 import ru.falseteam.schedule.R;
 import ru.falseteam.schedule.redraw.Redrawable;
 import ru.falseteam.schedule.redraw.Redrawer;
+import ru.falseteam.schedule.socket.Worker;
+import ru.falseteam.schedule.socket.commands.GetLessonNumbers;
+import ru.falseteam.schedule.socket.commands.GetLessons;
+import ru.falseteam.schedule.socket.commands.GetWeekDays;
 
 public class EditTemplateActivity extends AppCompatActivity implements Redrawable {
     private View emptyView;
@@ -23,6 +27,11 @@ public class EditTemplateActivity extends AppCompatActivity implements Redrawabl
 
         Redrawer.add(this);
         redraw();
+
+        Worker worker = Worker.get();
+        worker.send(GetWeekDays.getRequest());
+        worker.send(GetLessonNumbers.getRequest());
+        worker.send(GetLessons.getRequest());
     }
 
     @Override
@@ -36,7 +45,9 @@ public class EditTemplateActivity extends AppCompatActivity implements Redrawabl
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                if (true) {
+                if (GetWeekDays.weekDays != null &&
+                        GetLessonNumbers.lessonNumbers != null &&
+                        GetLessons.lessons != null) {
                     emptyView.setVisibility(View.GONE);
                     contentView.setVisibility(View.VISIBLE);
                 }
