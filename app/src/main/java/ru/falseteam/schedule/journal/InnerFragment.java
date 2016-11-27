be0e98dc3c35bbe12c55c80a9356f9e432031aea
Evgeny <evgeny@false.team>
2016-11-28 00:06:27 +0300

    something

---
+++ app/src/main/java/ru/falseteam/schedule/journal/InnerFragment.java
@@ -0,0 +1,137 @@
+package ru.falseteam.schedule.journal;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import ru.falseteam.schedule.R;
+import ru.falseteam.schedule.data.MainData;
+import ru.falseteam.schedule.listeners.Redrawable;
+import ru.falseteam.schedule.listeners.Redrawer;
+import ru.falseteam.schedule.serializable.JournalRecord;
+import ru.falseteam.schedule.socket.Worker;
+import ru.falseteam.schedule.socket.commands.GetJournal;
+
+public class InnerFragment extends Fragment implements Redrawable {
+    private java.sql.Date date;
+    private Adapter adapter;
+
+    public InnerFragment() {
+    }
+
+    private void openRecordEditor(JournalRecord record) {
+        Intent intent = new Intent(getActivity(), EditRecordActivity.class);
+        intent.putExtra("record", record);
+        startActivity(intent);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_templates, container, false);
+
+        TextView tv = (TextView) root.findViewById(R.id.day_of_week);
+        tv.setText(date.toString());
+
+        adapter = new Adapter(root.getContext(), new java.sql.Date(new Date().getTime()));
+        ListView list = (ListView) root.findViewById(R.id.list);
+        View view = root.findViewById(R.id.emptyView);
+        view.findViewById(R.id.progressBar).setVisibility(View.INVISIBLE);
+        list.setEmptyView(view);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                openRecordEditor(MainData.getJournal().get(position));
+            }
+        });
+
+        Redrawer.add(this);
+        redraw();
+
+        Worker.sendFromMainThread(GetJournal.getRequest());
+        return root;
+    }
+
+    @Override
+    public void redraw() {
+        adapter.update();
+    }
+
+    @Override
+    public void onDestroy() {
+        Redrawer.remove(this);
+        super.onDestroy();
+    }
+
+    public static InnerFragment newInstance(java.sql.Date date) {
+        InnerFragment fragment = new InnerFragment();
+        fragment.date = date;
+        return fragment;
+    }
+
+
+    private class Adapter extends BaseAdapter {
+        private Context context;
+        private List<JournalRecord> journal = new ArrayList<>();
+        private java.sql.Date date;
+
+        Adapter(Context context, java.sql.Date date) {
+            this.context = context;
+            this.date = date;
+            update();
+        }
+
+        void update() {
+            journal.clear();
+            for (JournalRecord record : MainData.getJournal())
+                if (record.date == date) journal.add(record);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return journal.size();
+        }
+
+        @Override
+        public JournalRecord getItem(int position) {
+            return journal.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null)
+                convertView = LayoutInflater.from(context)
+                        .inflate(R.layout.item_template, parent, false);
+            JournalRecord record = getItem(position);
+            ((TextView) convertView.findViewById(R.id.lesson_number)).setText(String.valueOf(record.lessonNumber.id));
+            ((TextView) convertView.findViewById(R.id.begin))
+                    .setText(record.lessonNumber.begin.toString().substring(0, 5));
+            ((TextView) convertView.findViewById(R.id.end))
+                    .setText(record.lessonNumber.end.toString().substring(0, 5));
+            ((TextView) convertView.findViewById(R.id.name)).setText(record.lesson.name);
+            ((TextView) convertView.findViewById(R.id.audience)).setText(record.lesson.audience);
+            return convertView;
+        }
+    }
+}
